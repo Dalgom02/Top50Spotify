0a60c7702449f31dc8362f7d9ff1707bc4a3e067
Your Name <you@example.com>
2023-09-02 21:20:09 -0400

    quick fixes

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,6 +1,6 @@
 async function getTopArtists(time_range, token) {
     try {
-        const response = await fetch(`https://top50spotify.vercel.app/api/top-artists?time_range=${time_range}`, {
+        const response = await fetch(`http://localhost:3000/api/top-artists?time_range=${time_range}`, {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
@@ -20,7 +20,7 @@ async function getTopArtists(time_range, token) {
 
 async function getTopTracks(time_range, token) {
     try {
-        const response = await fetch(`https://top50spotify.vercel.app/api/top-tracks?time_range=${time_range}`, {
+        const response = await fetch(`http://localhost:3000/api/top-tracks?time_range=${time_range}`, {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
